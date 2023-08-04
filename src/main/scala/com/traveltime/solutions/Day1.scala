d479092ce06be1b5ea13542f992660c66f791e53
Arnas Bradauskas <arnas.bradauskas@traveltime.com>
2023-08-04 17:07:42 +0300

    Solved day1

--- src/main/scala/com/traveltime/solutions/Day1.scala
+++ src/main/scala/com/traveltime/solutions/Day1.scala
@@ -1,3 +1,17 @@
 package com.traveltime.solutions
 
-object Day1 {}
+object Day1 {
+  def increasedDepthCountRec(
+      data: List[Int],
+      count: Int = 0,
+      index: Int = 0,
+      previousValue: Int = Int.MaxValue
+  ): Int = {
+    if (index == data.length) count
+    else {
+      if (data(index) > previousValue)
+        increasedDepthCountRec(data, count + 1, index + 1, data(index))
+      else increasedDepthCountRec(data, count, index + 1, data(index))
+    }
+  }
+}
