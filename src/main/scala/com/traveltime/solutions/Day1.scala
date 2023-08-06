82e5ada5b6167148f14b711db03911824d6c0bc7
Arnas Bradauskas <arnas.bradauskas@traveltime.com>
2023-08-06 23:06:55 +0300

    Used pattern matching to avoid an IndexOutOfBoundsException

--- src/main/scala/com/traveltime/solutions/Day1.scala
+++ src/main/scala/com/traveltime/solutions/Day1.scala
@@ -1,17 +1,21 @@
 package com.traveltime.solutions
 
+import scala.annotation.tailrec
+
 object Day1 {
+  @tailrec
   def part1(
       data: List[Int],
       count: Int = 0,
-      index: Int = 0,
       previousValue: Int = Int.MaxValue
   ): Int = {
-    if (index == data.length) count
-    else {
-      if (data(index) > previousValue)
-        part1(data, count + 1, index + 1, data(index))
-      else part1(data, count, index + 1, data(index))
+    data match {
+      case Nil => count
+      case head :: tail =>
+        if (head > previousValue)
+          part1(tail, count + 1, head)
+        else
+          part1(tail, count, head)
     }
   }
 
