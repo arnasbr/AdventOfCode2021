4818bdb5b854f0ea2f2a2d8fc3f8779aa1ad68ce
Arnas Bradauskas <arnas.bradauskas@traveltime.com>
2023-08-13 16:06:37 +0300

    Used fold instead of tailrec for Day6 part1

--- src/main/scala/com/traveltime/solutions/Day6.scala
+++ src/main/scala/com/traveltime/solutions/Day6.scala
@@ -13,22 +13,15 @@ object Day6 {
   private def parseIntList(input: String): Option[List[Int]] = {
     optionTraverse(input.split(",").map(_.toIntOption).toList)
   }
-  def part1(fish: Option[List[Int]], n: Int): Option[Int] = {
-    @tailrec
-    def processFishList(fishList: List[Int], times: Int): List[Int] = {
-      if (times <= 0) fishList
-      else {
-        val updatedList = fishList.foldLeft(List.empty[Int]) { (list, fish) =>
-          if (fish == 0) {
-            6 :: 8 :: list
-          } else fish - 1 :: list
-        }
-        processFishList(updatedList, times - 1)
-      }
-    }
 
+  def part1(fish: Option[List[Int]], n: Int): Option[Int] = {
     fish.map { fishList =>
-      val finalList = processFishList(fishList, n)
+      val finalList = (0 until n).foldLeft(fishList) { (currentFishList, _) =>
+        currentFishList.flatMap { fish =>
+          if (fish == 0) List(6, 8)
+          else List(fish - 1)
+        }
+      }
       finalList.length
     }
   }
@@ -38,6 +31,6 @@ object Day6 {
     val myInput =
       "3,5,1,2,5,4,1,5,1,2,5,5,1,3,1,5,1,3,2,1,5,1,1,1,2,3,1,3,1,2,1,1,5,1,5,4,5,5,3,3,1,5,1,1,5,5,1,3,5,5,3,2,2,4,1,5,3,4,2,5,4,1,2,2,5,1,1,2,4,4,1,3,1,3,1,1,2,2,1,1,5,1,1,4,4,5,5,1,2,1,4,1,1,4,4,3,4,2,2,3,3,2,1,3,3,2,1,1,1,2,1,4,2,2,1,5,5,3,4,5,5,2,5,2,2,5,3,3,1,2,4,2,1,5,1,1,2,3,5,5,1,1,5,5,1,4,5,3,5,2,3,2,4,3,1,4,2,5,1,3,2,1,1,3,4,2,1,1,1,1,2,1,4,3,1,3,1,2,4,1,2,4,3,2,3,5,5,3,3,1,2,3,4,5,2,4,5,1,1,1,4,5,3,5,3,5,1,1,5,1,5,3,1,2,3,4,1,1,4,1,2,4,1,5,4,1,5,4,2,1,5,2,1,3,5,5,4,5,5,1,1,4,1,2,3,5,3,3,1,1,1,4,3,1,1,4,1,5,3,5,1,4,2,5,1,1,4,4,4,2,5,1,2,5,2,1,3,1,5,1,2,1,1,5,2,4,2,1,3,5,5,4,1,1,1,5,5,2,1,1"
     val fish = parseIntList(input)
-    println(part1(fish, 256))
+    println(part1(fish, 80))
   }
 }
